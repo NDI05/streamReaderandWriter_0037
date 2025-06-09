1b1b1c2d3cd9cb7dff621ee9449404d7e76973af
Andhika Pratama <p21-andhika355@smkn7-smr.sch.id>
2025-06-09 10:31:54 +0700

    add code on try and first catch

--- manualExpection.cpp
+++ manualExpection.cpp
@@ -3,9 +3,11 @@ using namespace std;
 
 int main(){
     try{
-
+        cout << "Selamat Datang Di Prodi TI" << endl;
+        throw 0.5;
+        cout << "Pernyataan Tidak Akan Di Eksekusi" << endl;
     }catch (int a){
-
+        cout << "Pengecualian Akan Di Eksekusi" << endl;
     }catch (...){
 
     };
