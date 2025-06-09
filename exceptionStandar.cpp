9aa5a537c551a79b072cfa4d3edb498bd328e32e
Andhika Pratama <p21-andhika355@smkn7-smr.sch.id>
2025-06-09 10:39:42 +0700

    create try catch

--- exceptionStandar.cpp
+++ exceptionStandar.cpp
@@ -4,5 +4,14 @@
 using namespace std;
 
 int main(){
-
+    cout << "Awal Program" << endl;
+    try
+    {
+        
+    }
+    catch(exception& e)
+    {
+        cout << e.what() << endl;
+    }
+    
 };
